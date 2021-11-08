71456808a4a4c4b51e7bdc943138c74f612b5867
ElenaMughi <elena.mughi@gmail.com>
2021-11-08 23:11:10 +0300

    TotalClientBalance ver01

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int CurrentClientBalance = 2_000_000_000;
+        int TransferSumma = 500_000_000;
+        int Total = CurrentClientBalance + TransferSumma;
+        System.out.println(Total);
+    }
+}
