b04b292059f9123ad1e8af0c55f2ac7fab11d1b1
ElenaMughi <91494429+ElenaMughi@users.noreply.github.com>
2021-11-13 15:48:19 +0300

    Fix var

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        int CurrentClientBalance = 2_000_000_000;
-        int TransferSumma = 500_000_000;
-        int Total = CurrentClientBalance + TransferSumma;
-        System.out.println(Total);
+        int currentclientbalance = 2_000_000_000;
+        int transfersumma = 500_000_000;
+        int total = currentclientbalance + transfersumma;
+        System.out.println(total);
     }
 }
